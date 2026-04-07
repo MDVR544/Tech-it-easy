8aab18b497b27a97500de3e136de8d5f854091df
MDVR544 <devriescool@gmail.com>
2026-04-07 16:59:09 +0200

    start opdracht 1a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css';
 
+import {inventory} from "./constants/inventory.js";
+
 function App() {
+const result = inventory;
+  let totalSold = 0
+
+  for (let i = 0; i < inventory.length; i++) {
+    // console.log(inventory[i].sold);
+    totalSold += inventory[i].sold;
+  }
   return (
+      <>
     <h1>Begin hier met met maken van de applicatie!</h1>
+    <p>totaal verkocht {totalSold}</p>
+      </>
   )
+
 }
 
 export default App
---
+++ src/Helpers/TotalSales/TotalSales.js
@@ -0,0 +1,4 @@
+//Opdracht 1a: Hoeveel tv's zijn er al verkocht? Schrijf een helperfunctie die dit berekent. Log de uitkomst in de console.
+import {inventory} from 'src/constants/inventory.js'
+
+console.log(inventory);
